69c7c863922f0e5d857e06cbf04345cbf19d7e1f
Zsolt Szende <szendezs@gmail.com>
2017-06-19 12:39:33 +0100

    Make the transition function monadic to truly decouple it from the fsm

--- fsm.hs
+++ fsm.hs
@@ -3,34 +3,33 @@ module FSM where
 
 import Control.Monad (foldM)
 
-data State = Asleep | Awake | Dressed | Fed | Ready | Work | Cafe | Home | Invalid
+data State = Awake | Dressed | Fed | Ready | Work | Cafe | Home | Asleep
     deriving (Show, Eq)
 
 data Event = Alarm | Dress | Eat | GoToWork | Think | Yawn | Coffee | GoHome | Read | WatchTV
     deriving (Show, Eq)
 
 -- Transition function (aka. transition table)
-myDay :: (State, Event) -> State
-myDay (Asleep,  Alarm)    = Awake
-myDay (Awake,   Dress)    = Dressed
-myDay (Awake,   Eat)      = Fed
-myDay (Dressed, Eat)      = Ready
-myDay (Fed,     Dress)    = Ready
-myDay (Ready,   GoToWork) = Work
-myDay (Work,    Think)    = Work
-myDay (Work,    Yawn)     = Cafe
-myDay (Cafe,    Coffee)   = Work
-myDay (Work,    GoHome)   = Home
-myDay (Home,    Read)     = Asleep
-myDay (Home,    WatchTV)  = Asleep
-myDay (_, _)              = Invalid
+myDay :: (State, Event) -> Maybe State
+myDay (Asleep,  Alarm)    = Just Awake
+myDay (Awake,   Dress)    = Just Dressed
+myDay (Awake,   Eat)      = Just Fed
+myDay (Dressed, Eat)      = Just Ready
+myDay (Fed,     Dress)    = Just Ready
+myDay (Ready,   GoToWork) = Just Work
+myDay (Work,    Think)    = Just Work
+myDay (Work,    Yawn)     = Just Cafe
+myDay (Cafe,    Coffee)   = Just Work
+myDay (Work,    GoHome)   = Just Home
+myDay (Home,    Read)     = Just Asleep
+myDay (Home,    WatchTV)  = Just Asleep
+myDay (_, _)              = Nothing
 
 -- The machine decoupled from the specific transition function
-fsm :: ((State, Event) -> State) -> (State, Event) -> Maybe State
-fsm t = \(s, e) -> case t (s, e) of
-    Invalid  -> Nothing
-    newState -> Just newState
+fsm :: ((State, Event) -> Maybe State) -> (State, Event) -> Maybe State
+fsm t (s, e) = t (s, e)
 
 -- Running the simulation with a transition function, an initial state and a list of events
-run :: ((State, Event) -> State) -> State -> [Event] -> Maybe State
+run :: ((State, Event) -> Maybe State) -> State -> [Event] -> Maybe State
+--run t s = foldl (curry $ fsm t) s
 run t s = foldM (curry $ fsm t) s
